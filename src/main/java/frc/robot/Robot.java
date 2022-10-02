2cb26c812dd6d93dda4965a043313025a934a04c
Therese Bunyi <thereseb@Thereses-MacBook-Pro.local>
2022-10-02 14:12:52 -0700

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the methods corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot
+{
+  WPI_TalonSRX motor1 = new WPI_TalonSRX(1);
+  Joystick joystick = new Joystick(0);
+
+
+  @Override
+  public void teleopPeriodic() {
+    if(joystick.getRawButton(3)) {
+      motor1.set(0.5);;
+    }
+    if(!joystick.getRawButton(3)) {
+      motor1.set(0);
+    }
+
+  }
+}
