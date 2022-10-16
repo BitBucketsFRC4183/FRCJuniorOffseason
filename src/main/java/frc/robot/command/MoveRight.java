ececd7888679785dc8c2b4aaa995413cb1e52bc3
Ibro-cyber <71227338+Ibro-cyber@users.noreply.github.com>
2022-10-16 15:04:44 -0700

    autonomousCode

---
+++ src/main/java/frc/robot/command/MoveRight.java
@@ -0,0 +1,25 @@
+package frc.robot.command;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.DriveSubsystem;
+
+public class MoveRight extends CommandBase{
+
+    int counter = 0;
+    final DriveSubsystem driveSubsystem;
+
+    public MoveRight(DriveSubsystem driveSubsystem) {
+        this.driveSubsystem = driveSubsystem;
+    }
+
+    @Override
+    public boolean isFinished() {
+        counter += 1;
+        return (counter >= 2000);
+    }
+
+    @Override
+    public void execute() {
+        driveSubsystem.turnRight();
+    }
+}
